a5d3a04dc1cff588edf8afe69463abf39dced6d7
samkhune9921 <sambhajikhune12@gmail.com>
2022-10-12 06:57:52 +0000

    newline added

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
-echo "Hello World!";
-echo "hello from dev!";
-echo "Edit from monitoring!";
-echo "good morning!";
-echo "Hello!";
-echo "Hi!";
-echo "Good Evening";
-echo "Hello friend";
-echo "hi this comment is from dev";
-echo "hello!!!!";
+echo "Hello World!\n";
+echo "hello from dev!\n";
+echo "Edit from monitoring!\n";
+echo "good morning!\n";
+echo "Hello!\n";
+echo "Hi!\n";
+echo "Good Evening\n";
+echo "Hello friend\n";
+echo "hi this comment is from dev\n";
+echo "hello!!!!\n";
 ?>
